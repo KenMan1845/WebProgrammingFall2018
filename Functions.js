e31d9e95ae452d8aa4db3ff1b8ca2f3322d0f7ac
KennethM <kmanning251@gmail.com>
2018-10-16 22:36:31 -0400

    started API and app development

---
+++ Functions.js
@@ -0,0 +1,17 @@
+var options = {
+    valueNames: [ 'name']
+  };
+  
+  var values = [ {
+    name: "Kenny Manning",
+    
+  }];
+  
+  var userList = new List('users', options, values);
+  
+  userList.add({
+    name: "Moshe Plotkin"
+    
+  });
+
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const game = require('./Functions');
+
+const app = express();
+
+const port = 3000;
+const server = "localhost";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../client/"));
+app.use('/game', game);
+
+app.listen(port);
+
+console.log(`listening on: http://${server}:${port}`);
